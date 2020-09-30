6d16887fcc1eee5907ace3c2d716e5c3d8e3cf2e
Basile Durand de Gevigney <Basile.gevigney@free.fr>
2020-09-30 11:46:35 +0200

    it's works :tada:

--- frac.c
+++ frac.c
@@ -1,34 +1,40 @@
  /*
  **  Author: Basile Durand de Gevigney EI2I3 II
- **  Create Time: 2020-09-17 17:10:17
+ **  Create Time: 2020-09-30 07:44:56
  **  Modified by: Basile Durand de Gevigney
- **  Modified time: 2020-09-18 12:42:17
+ **  Modified time: 2020-09-30 11:44:42
  **  Description: none
  */
-
 #include <stdio.h>
-#define PRECISION 10000000
+#include <stdlib.h>
+
+void myfrac(int x, int y)
+{
+    int d;
+
+    d = 0;
+    while (x != 0)
+        if (x * ++d >= y)
+        {
+            printf(" + 1/%d", d);
+            if ((y % d) == 0)
+                x -= y/d;
+            else
+            {
+                x = (x * d) - y;
+                y = d * y;
+            }
+        } 
+}
 
 int main()
 {
-int a;
-int d;
-int b;
-long double nbr;
+    int x;
+    int y;
 
-scanf("%d/%d",&a,&b);
-nbr = a/(long double)b;
-d = 1;
-printf("%i/%i =", a, b);
-while (nbr > 0 && d < PRECISION - 1)
-{
-    //printf(" + %i >= %i ?", ((unsigned int)(nbr * PRECISION)), (unsigned int)((1/(long double)d)* PRECISION));
-    if (((unsigned int)(nbr * PRECISION )) >= (unsigned int)((1/(long double)d)* PRECISION))
-    {
-        nbr -= 1/(long double)d;
-        printf(" + %i/%i", 1, d);
-    }
-    d++;
-}
-printf("\n");
+    scanf("%d/%d", &x, &y);
+    printf("%d/%d = ", x, y);
+    myfrac(x, y);
+    printf("\n");
+    exit (EXIT_SUCCESS);
 }
